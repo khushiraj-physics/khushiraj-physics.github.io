25d3566f09948a031d1ea1cc8017f741982ee1d5
Khushi349 <230406khushi@gmail.com>
2026-06-02 22:41:07 +0530

    force all actions to node 24 to clear warnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 	site: 'https://khushiraj-physics.github.io',
 	integrations: [mdx(), sitemap()],
-	markdown: {
-		drafts: true
-	}
+	fonts: [
+		{
+			name: 'Atkinson Hyperlegible',
+			cssVariable: '--font-atkinson',
+			provider: fontProviders.google(),
+		}
+	]
 });
