40aa86b6fdffb270417a3b3ce9bdd467b7ffde53
Khushi349 <230406khushi@gmail.com>
2026-06-02 22:30:49 +0530

    use standard github node actions setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,8 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://khushiraj-physics.github.io',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		// Prevents hidden Mac system files from crashing your build
+		drafts: true
+	}
 });
