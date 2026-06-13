c40dde5699bc50d8de56711fdcf43eea13cab78d
Khushi349 <230406khushi@gmail.com>
2026-06-13 17:23:33 +0530

    Google verification

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Khushi Raj';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Physics Student and Researcher';
